3a6087030ec84b33a31aebb4f6b758898daee1fa
gsd1401@gmail.com <gsd1401@gmail.com>
2020-03-13 15:19:19 +0530

    Button in welcome

--- DBMS_FitnessTracker/Welcome.Designer.cs
+++ DBMS_FitnessTracker/Welcome.Designer.cs
@@ -29,25 +29,38 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(298, 69);
+            this.label1.Location = new System.Drawing.Point(265, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.Size = new System.Drawing.Size(103, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to FT";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Form1
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.button1.Location = new System.Drawing.Point(280, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HELLO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Welcome";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -57,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DBMS_FitnessTracker/Welcome.cs
+++ DBMS_FitnessTracker/Welcome.cs
@@ -19,7 +19,12 @@ namespace DBMS_FitnessTracker
 
         private void label1_Click(object sender, EventArgs e)
         {
+           
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
