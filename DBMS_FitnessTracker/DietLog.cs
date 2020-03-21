44871a9965059abaa24ffe60a1176b2dfa705157
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-03-21 16:13:10 +0530

    new diet log

---
+++ DBMS_FitnessTracker/DietLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMS_FitnessTracker
+{
+    public partial class DietLog : Form
+    {
+        public DietLog()
+        {
+            InitializeComponent();
+        }
+    }
+}
