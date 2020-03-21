68ba5928c09f6a4f2f760815d76382bd522359bc
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-03-21 19:05:01 +0530

    Backend of user page

---
+++ DBMS_FitnessTracker/CumulativeReport.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace DBMS_FitnessTracker
+{
+    partial class CumulativeReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(74, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(961, 147);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(74, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(338, 239);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Numbers";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(467, 251);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(568, 239);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Graphs";
+            // 
+            // CumulativeReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 649);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CumulativeReport";
+            this.Text = "CumulativeReport";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
--- DBMS_FitnessTracker/UserPage.cs
+++ DBMS_FitnessTracker/UserPage.cs
@@ -1,14 +1,90 @@
 ﻿using System.Windows.Forms;
-
+using System.Collections.Specialized;
+using System;
+using MySql.Data.MySqlClient;
 namespace DBMS_FitnessTracker
 {
+   
+
+
     public partial class UserPage : Form
     {
+        public static string constr = "server=localhost;user id=root;database=ft;CharSet=utf8;persistsecurityinfo=True;password=swetha2000";
+        MySqlConnection con1 = new MySqlConnection(constr);
+
         public UserPage()
         {
             InitializeComponent();
         }
 
+        public int findUID()
+        {
+            try
+            {
+                con1.Open();
+                string Query = "select max(userid)+1 from user";
+                MySqlCommand cmd = new MySqlCommand(Query, con1);
+                MySqlDataReader dr = cmd.ExecuteReader();
+               
+                if(dr.Read())
+                {
+                    con1.Close();
+                    return dr.GetUInt16(0);
 
+                }
+               
+
+
+            }
+            catch(Exception er)
+            {
+                con1.Close();
+                return 1;
+            }
+
+            con1.Close();
+            return 1;
+
+
+        }
+        private void Save_Click(object sender, System.EventArgs e)
+        {
+            string gender = string.Empty;
+            if (male.Checked && female.Checked)
+                MessageBox.Show("Select one gender");
+            if (male.Checked)
+                gender = "Male";
+            else if (female.Checked)
+                gender = "Female";
+            else
+                MessageBox.Show("Please select a gender:");
+            string age = agebox.Value.ToString();
+            string height = ht.Value.ToString();
+            string weight = wt.Value.ToString();
+           
+            string Query = "insert into user (userid,name,age,gender,phoneNo,email,height,weight,category) values ("+findUID()+",'" + name.Text + "'," + age + ",'" + gender + "','" + phone.Text + "','" + email.Text + "'," + height + "," + weight +",'"+ category.SelectedItem.ToString()+"');";
+            MySqlCommand cmd = new MySqlCommand(Query, con1);
+
+
+            try 
+            {
+                
+                con1.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Saved Successfully");
+
+
+            }
+            catch (MySqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+            con1.Close();
+
+        }
     }
 }
