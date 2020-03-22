4fd1cbbf5c4a787a0b327ad5756d19a10f5e30fe
iswaryaagp@gmail.com <iswaryaagp@gmail.com>
2020-03-22 16:01:04 +0530

    dietlog

--- DBMS_FitnessTracker/DietLog.cs
+++ DBMS_FitnessTracker/DietLog.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace DBMS_FitnessTracker
 {
     public partial class DietLog : Form
@@ -15,6 +15,46 @@ namespace DBMS_FitnessTracker
         public DietLog()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DietLog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           
         }
     }
 }
