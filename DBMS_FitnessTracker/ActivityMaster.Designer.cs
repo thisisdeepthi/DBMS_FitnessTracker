da62b888e6b701e2dcfacc0d95ccdb38eb5afa04
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-05-11 17:39:37 +0530

    Activity Master and Activity Log Complete

--- DBMS_FitnessTracker/ActivityMaster.Designer.cs
+++ DBMS_FitnessTracker/ActivityMaster.Designer.cs
@@ -30,21 +30,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cal = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SAVE = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.groupBox1.Location = new System.Drawing.Point(53, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 350);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Existing Activities";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.SAVE);
+            this.groupBox2.Controls.Add(this.cal);
+            this.groupBox2.Controls.Add(this.nameBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.groupBox2.Location = new System.Drawing.Point(423, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 350);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add New Activity";
+            
+            // 
+            // cal
+            // 
+            this.cal.Location = new System.Drawing.Point(207, 123);
+            this.cal.Name = "cal";
+            this.cal.Size = new System.Drawing.Size(100, 26);
+            this.cal.TabIndex = 3;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(207, 70);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 26);
+            this.nameBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(51, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 49);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Calories burnt per MIN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Activity Name";
+            // 
+            // SAVE
+            // 
+            this.SAVE.Location = new System.Drawing.Point(119, 227);
+            this.SAVE.Name = "SAVE";
+            this.SAVE.Size = new System.Drawing.Size(98, 55);
+            this.SAVE.TabIndex = 4;
+            this.SAVE.Text = "Add Activity";
+            this.SAVE.UseVisualStyleBackColor = true;
+            this.SAVE.Click += new System.EventHandler(this.SAVE_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(33, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 244);
+            this.listBox1.TabIndex = 0;
             // 
             // ActivityMaster
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 338);
+            this.ClientSize = new System.Drawing.Size(1132, 520);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ActivityMaster";
             this.Load += new System.EventHandler(this.ActivityMaster_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox cal;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SAVE;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
